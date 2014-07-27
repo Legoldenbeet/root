55adc9f39563bbbb531172918f8d8e1afe5b62f0
xiaoliyad <xiaoliyad@163.com>
2014-07-27 20:40:41 +0800

    test

--- CardPersonalizationAssistantTool/src/com/gerenhua/tool/panel/AbstractPanle.java
+++ CardPersonalizationAssistantTool/src/com/gerenhua/tool/panel/AbstractPanle.java
@@ -12,7 +12,6 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
-import org.springframework.context.ApplicationContext;
 /**
  * 
  * @description: 抽象功能模板
@@ -22,74 +21,70 @@ import org.springframework.context.ApplicationContext;
  * @Copyright: watchdata
  */
 public class AbstractPanle extends JPanel {
-	
+
 	private static final long serialVersionUID = 1L;
-	private final SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
-	
-	protected ApplicationContext appContext;
-	protected TextPaneMenu logTextArea;	
+	private final SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
+
+	protected TextPaneMenu logTextArea;
 	protected JScrollPane jsPan;
-	
+
 	private static int color = 0;
-	
-	private SimpleAttributeSet attrSetOdd;   //奇数行属性
-	private SimpleAttributeSet attrSetEven;  //偶数行属性
-	private SimpleAttributeSet attrSetError; //错误日志属性
-	
-	
-	public AbstractPanle(){
+
+	private SimpleAttributeSet attrSetOdd; // 奇数行属性
+	private SimpleAttributeSet attrSetEven; // 偶数行属性
+	private SimpleAttributeSet attrSetError; // 错误日志属性
+
+	public AbstractPanle() {
 		super();
-		logTextArea = new TextPaneMenu();		
-		//logTextArea.setEditable(false);
-	
+		logTextArea = new TextPaneMenu();
+		// logTextArea.setEditable(false);
+
 		jsPan = new JScrollPane(logTextArea);
 		jsPan.setViewportView(logTextArea);
-		
+
 		attrSetOdd = new SimpleAttributeSet();
-		attrSetEven = new SimpleAttributeSet();		
-		attrSetError = new SimpleAttributeSet();		
+		attrSetEven = new SimpleAttributeSet();
+		attrSetError = new SimpleAttributeSet();
 		StyleConstants.setForeground(attrSetOdd, Color.blue);
 		StyleConstants.setForeground(attrSetEven, Color.black);
 		StyleConstants.setForeground(attrSetError, Color.red);
 		StyleConstants.setBold(attrSetError, true);
-		
+
 	}
-	
-	public AbstractPanle(ApplicationContext appContext){
-		this();			
-		this.appContext = appContext;
-	}
-	
+
 	/**
 	 * 增加一行普通日志，隔行隔色
+	 * 
 	 * @param logStr
 	 */
-	public void addLog(String logStr){		
-		String info = sf.format(new Date())+logStr +"\n";	
-		if(AbstractPanle.color==0){
-			addLog(info,logTextArea,attrSetEven);
+	public void addLog(String logStr) {
+		String info = sf.format(new Date()) + logStr + "\n";
+		if (AbstractPanle.color == 0) {
+			addLog(info, logTextArea, attrSetEven);
 			AbstractPanle.color = 1;
-		}else{
-			addLog(info,logTextArea,attrSetOdd);
+		} else {
+			addLog(info, logTextArea, attrSetOdd);
 			AbstractPanle.color = 0;
 		}
-		
+
 	}
+
 	/**
 	 * 增加一行错误日志，显示红色粗体
+	 * 
 	 * @param logStr
 	 */
-	public void addErrorLog(String logStr){		
-		String info = sf.format(new Date())+logStr +"\n";			
-		addLog(info,logTextArea,attrSetError);
-		
+	public void addErrorLog(String logStr) {
+		String info = sf.format(new Date()) + logStr + "\n";
+		addLog(info, logTextArea, attrSetError);
+
 	}
-	
+
 	private void addLog(String text, JTextPane textPane, SimpleAttributeSet attr) {
 		StyledDocument doc = textPane.getStyledDocument();
 		try {
 			doc.insertString(doc.getLength(), text, attr);
-		} catch (BadLocationException e) {			
+		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
 	}
