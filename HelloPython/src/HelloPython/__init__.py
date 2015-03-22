b83543b777e7283166e15b972adeac7eb0297672
kingofglassheart <kingofglassheart@192.168.0.103>
2015-03-22 21:28:09 +0800

    hello python1

---
+++ HelloPython/src/HelloPython/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+
+print("helloworld")
+print(os.name)
+print(os.sep)
+#os.system("cmd.exe")
+print(os.getpid())
+print(os.getenv("java_home", "eh"))
+print(os.getcwd())
+print(os.getcwdu())
+print(os.curdir)
+print("hel:"+os.linesep)
+
+print(os.path.exists("G:\\1000_hotel_csv.csv"))
+print(os.path.isfile("G:\\1000_hotel_csv.csv"))
+
+name,ext=os.path.splitext("G:\\1000_hotel_csv.csv")
+print(name);
+print(ext);
+
+L=os.listdir("G:\\")
+print L
+
+os.makedirs("G:\\1234\\456")
