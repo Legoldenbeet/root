a30c2ae40f817886b817c8f560267b5111448dd4
xiaoliyad <xiaoliya2011@163.com>
2016-02-16 15:18:51 +0800

    del src of ftp4j

--- FTPApp/src/com/shuishuo/ftp4j/FTP.java
+++ FTPApp/src/com/shuishuo/ftp4j/FTP.java
@@ -11,19 +11,27 @@ import it.sauronsoftware.ftp4j.FTPListParseException;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
 public class FTP {
 	public static void main(String[] args) throws IllegalStateException, IOException, FTPIllegalReplyException, FTPException, FTPDataTransferException, FTPAbortedException, FTPListParseException {
 		FTPClient ftp = new FTPClient();
 		ftp.connect("ftp.watchdata.com", 21);
 		ftp.login("rk", "rk^&21");
+		ftp.setCharset("GBK");
 		ftp.setPassive(true);
+
 		String dir = ftp.currentDirectory();
 		System.out.println("currentDirectory:" + dir);
-		// String[] ftpFileNames=ftp.listNames();
-		// for (String ftpFileName : ftpFileNames) {
-		// System.out.println(ftpFileName);
-		// }
-
+//		String[] ftpFileNames = ftp.listNames();
+//		for (String ftpFileName : ftpFileNames) {
+//			System.out.println(ftpFileName);
+//		}
+		
+		File fileDir = new File("E:/baidu player/DBS/");
+		FileUtils.deleteDirectory(fileDir);
+		fileDir.mkdirs();
+		
 		FTPFile[] ftpFiles = ftp.list();
 		for (FTPFile ftpFile : ftpFiles) {
 			int fileType = ftpFile.getType();
